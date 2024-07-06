b3dcb03676042e4e55003f951da027e01395d2e9
ser356 <sergiogarciaseco@usal.es>
2024-07-06 23:01:35 +0200

    macos iii

--- build-aux/macos-build.sh
+++ build-aux/macos-build.sh
@@ -2,10 +2,7 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source $HOME/.cargo/env
 
-rustup target add x86_64-apple-darwin
 
 
 brew install meson
