fff03ff21cc2683c14c1e719d1c84842acf25a48
ser356 <sergiogarciaseco@usal.es>
2024-07-06 22:57:21 +0200

    macos ii

--- build-aux/macos-build.sh
+++ build-aux/macos-build.sh
@@ -5,6 +5,9 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 
+rustup target add x86_64-apple-darwin
+
+
 brew install meson
 brew install gtk4
 brew install gtksourceview5 || (brew install svn && brew install gtksourceview5)
@@ -15,6 +18,22 @@ brew install libadwaita
 export GETTEXT_DIR=$(brew --prefix)/opt/gettext
 
 
-meson setup build --prefix=/usr
+meson setup build --prefix="/" -Dprofile=development
 ninja -C build
 ninja -C build install
+
+BIN="mi_app"
+BIN_PATH="build/src/${BIN}"
+
+mkdir -p dist
+
+cp "${BIN_PATH}" dist/
+
+DEPENDENCIAS=$(otool -L "${BIN_PATH}" | awk '/\/usr\/local\/opt/{print $1}' | sort | uniq) 
+for dep in $DEPENDENCIAS; do
+    dep_basename=$(basename "$dep")
+    
+    cp "$dep" dist/
+    
+    install_name_tool -change "$dep" "@executable_path/${dep_basename}" "dist/${BIN}"
+done
